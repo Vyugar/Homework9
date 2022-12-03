05b9971eb0503f59c4c34d4ec165779d55c4c6ed
Vyugar <vyugar@yandex.ru>
2022-12-03 21:29:55 +0300

    modified Task064

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -8,30 +8,30 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
 Console.WriteLine("Введите натуральное число больше 1:");
-int NumberN = Convert.ToInt32(Console.ReadLine());
+int numberN = Convert.ToInt32(Console.ReadLine());
 
-void NumberCounter(int NumberN)
+void NumberCounter(int numberN)
 {
-    if (NumberN < 0)
+    if (numberN < 0)
     {
-        Console.WriteLine($"{NumberN} не натуральное число");
+        Console.WriteLine($"{numberN} не натуральное число");
         return;
     }
-    if (NumberN == 0) return;
+    if (numberN == 0) return;
     /*
     Др. вариант:
-    if (NumberN == 0)
+    if (numberN == 0)
     {
         Console.WriteLine($"Вы ввели {NumberN}, который меньше 1");
         return;
     }
     */
 
-    else if (NumberN > 0)
+    else if (numberN > 0)
     {
-        Console.Write("{0,4}", NumberN);
-        NumberCounter(NumberN - 1);
+        Console.Write("{0,4}", numberN);
+        NumberCounter(numberN - 1);
     }
 }
 
-NumberCounter(NumberN);
+NumberCounter(numberN);
